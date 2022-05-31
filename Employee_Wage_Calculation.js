54a677b80dc121090e8b8a2807221bcc29203770
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-31 23:13:29 +0530

    Added: Fifth case to calculate the total Employee wages for a month

--- Employee_Wage_Calculation.js
+++ Employee_Wage_Calculation.js
@@ -1,5 +1,5 @@
-//======================================== UC4 =======================================================================//
-//To Calculating Wages for a Month assuming 20Working Days in a Month
+//======================================== UC5 =======================================================================//
+//To calculate the wages till the number of working hours per month is reached.
 const IS_ABSENT = 0;
 const IS_FULL_TIME = 1;
 const IS_PART_TIME = 2;
@@ -7,24 +7,29 @@ const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+const MAX_DAYS_IN_MONTH = 20;
 
 function getWorkingHours(empCheck){
     switch (empCheck){
     case IS_ABSENT:
     return 0;
-        
+
     case IS_FULL_TIME:
     return FULL_TIME_HOURS;
-        
+
     case IS_PART_TIME:
     return PART_TIME_HOURS;
     }
 }
 let empHours = 0;
+let totalWorkingHours = 0;
+let totalWorkingDays = 0;
 
-for (let i = 0; i < NUM_OF_WORKING_DAYS; i++) {
+while (totalWorkingDays < MAX_DAYS_IN_MONTH || totalWorkingHours < MAX_HRS_IN_MONTH) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random()*10)%3;
-    empHours += getWorkingHours(empCheck);
+    totalWorkingHours += getWorkingHours(empCheck);
 }
-let empWage = empHours * WAGE_PER_HOUR;
-console.log("Total  working Hours is : "+empHours+ " &" + " Employee wage is : "+ empWage);
+let empWage = totalWorkingHours * WAGE_PER_HOUR;
+console.log("Total Days: "+totalWorkingDays +" & Total Hours: "+totalWorkingHours+ "  & Employee wage: "+ empWage);
