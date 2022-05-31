bab9fd10ad26acddc09c814c113c718ecbcd465f
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-31 22:05:16 +0530

    Added: First case to check Employee attendance 1~

--- Employee_Wage_Calculation.js
+++ Employee_Wage_Calculation.js
@@ -1,1 +1,11 @@
-console.log("*** Welcome to the Employee Wages Program ***");
+//======================================== UC1 =======================================================================//
+//To check Employee availability (Present or absent)
+ const IS_ABSENT =0;
+ 
+ let empCheck = Math.floor(Math.random() * 10 ) % 2;
+ if (empCheck == IS_ABSENT){
+     console.log("Employee is Absent");
+    
+ } else {
+    console.log("Employee is Present");
+ }
