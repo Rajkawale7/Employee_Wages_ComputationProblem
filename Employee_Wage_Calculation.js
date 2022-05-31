4cc9c6ca0112fc2fe1e2197402665034acd3b3bc
Rajkawale7 <Rajkawale712@gmail.com>
2022-05-31 22:30:41 +0530

    Added: Third case to check the Daily Employee working hours

--- Employee_Wage_Calculation.js
+++ Employee_Wage_Calculation.js
@@ -1,23 +1,15 @@
-//======================================== UC2 =======================================================================//
-//To Calculate Daily Employee Wage based on part time or full time work
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
-
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck){
-   case IS_PART_TIME:
-       empHrs = PART_TIME_HOURS;
-       break;
-   case IS_FULL_TIME:
-       empHrs = FULL_TIME_HOURS;
-       break
-   default:
-       empHrs = 0;
+//======================================== UC3 =======================================================================//
+//Refactored code to write function for daily working Hours
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("The employee wage is: " + empWage);
+    empHrs = getWorkingHours(empCheck);
+    console.log("Emp wage: " + empWage);
