21f501b46e01af62d0d640ad9e0392a5fc9ee3dd
Rajkawale7 <Rajkawale712@gmail.com>
2022-06-03 15:04:22 +0530

    Added: Tweleth case to extend the Employee Payroll class

---
+++ Employee_Payroll_Service.js
@@ -0,0 +1,43 @@
+// UC12 - Ability to extendEmployee Payroll Data to store gender and start date
+
+class EmployeePayrollData
+{
+    //Properties
+	id;
+	salary;
+	gende;
+	startDate;
+
+    //Constructor
+	constructor(...params){
+		this.id=params[0];
+		this.name=params[1];
+		this.salary=params[2];
+		this.gender=params[3];
+		this.startDate=params[4];
+	}
+
+    //Getting values by using getter and setter method
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        this._name=name;
+    }
+    toString() {
+        const options= { year:'numeric', month: 'long', day: 'numeric'};
+        const empDate= this.startDate === undefined ? "undefined": 
+                       this.startDate.toLocaleDateString("en-US",options);
+
+        return "id=" + this.id+", name=" + this.name +", salary=" + this.salary + ", " +
+             "gender=" + this.gender+", startDate ="+ empDate;
+    }
+
+}
+
+let employeePayrollData = new EmployeePayrollData(1,"Mark",30000);
+console.log(employeePayrollData.toString());
+employeePayrollData.name="john";
+console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
